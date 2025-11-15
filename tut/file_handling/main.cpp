d7e4ee05bba48f7e5fd4a496cadb51625de5842b
Armaan <armaanbhatoya390@gmail.com>
2025-11-15 23:19:07 +0530

    modified

--- tut/file_handling/main.cpp
+++ tut/file_handling/main.cpp
@@ -7,12 +7,15 @@ int main(){
 
     string st = "Armaan";
     string st2;
-    // opening file using constructor and writing it
-    ofstream out("sample.txt"); //write operation
-    out<<st;
+    // // opening file using constructor and writing it
+    // ofstream out("sample.txt"); //write operation
+    // out<<st;
 
     ifstream in("sample.txt"); //opening file using constructor and reading it
     in>>st2;
     cout<<st2;
+    getline(in, st2); //it gets the whole line with spaces
+    cout<<st2;
+    
     return 0;
 }
---
+++ tut/file_handling/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+ // // opening file using constructor and writing it
+    // ofstream out("sample.txt"); //write operation
+    // out<<st;
