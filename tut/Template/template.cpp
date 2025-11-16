b07f55fe0a7afec1a2d7d140d3bcce8226124231
Armaan <armaanbhatoya390@gmail.com>
2025-11-16 17:13:29 +0530

    modify

--- tut/Template/template.cpp
+++ tut/Template/template.cpp
@@ -2,17 +2,18 @@
 
 using namespace std;
 
+template<class T>
 class vector{
     public:
-        int * arr;
+        T * arr;
         int size;
         
-        vector(int m){
+        vector(T m){
             size = m;
-            arr = new int[size];
+            arr = new T[size];
         }
-        int dotProduct(vector &v){
-            int d = 0;
+        T dotProduct(vector &v){
+            T d = 0;
             for(int i=0; i<size; i++){
                 d += this->arr[i] * v.arr[i];
             }
@@ -21,17 +22,16 @@ class vector{
 };
 
 int main(){
-    vector v1(3);
-    v1.arr[0] = 4;
-    v1.arr[1] = 3;
-    v1.arr[2] = 2;
+    vector <float> v1(3);
+    v1.arr[0] = 1.4;
+    v1.arr[1] = 3.3;
+    v1.arr[2] = .1;
     
-    vector v2(3);
-    v2.arr[0]=2;
-    v2.arr[1]=3;
-    v2.arr[2]=4;
+    vector<float> v2(3);
+    v2.arr[0]=.1;
+    v2.arr[1]=1.90;
+    v2.arr[2]=4.1;
 
-    int a = v1.dotProduct(v2);
-    cout<< a<<endl;
+    cout<< v1.dotProduct(v2)<<endl;
     return 0;
 }
