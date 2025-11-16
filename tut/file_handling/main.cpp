46439c1acbdaa8481754aef99aac4214391d674f
Armaan <armaanbhatoya390@gmail.com>
2025-11-16 14:52:00 +0530

    modified

--- tut/file_handling/main.cpp
+++ tut/file_handling/main.cpp
@@ -19,18 +19,38 @@ int main(){
 
     // -------------------- part 2 ---------------------------------
 
-    ofstream hout("sample.txt");
-    string st;
-    cout<<"Enter your name : ";
-    getline(cin,st);
-    hout<<"My name is "<<st;
-    hout.close();
+    // ofstream hout("sample.txt");
+    // string st;
+    // cout<<"Enter your name : ";
+    // getline(cin,st);
+    // hout<<"My name is "<<st;
+    // hout.close();
 
-    ifstream hin("sample.txt");
-    string content;
-    // hin>>content;
-    getline(hin,content);
-    cout<<"This is the content in the file : "<<content<<endl;
+    // ifstream hin("sample.txt");
+    // string content;
+    // // hin>>content;
+    // getline(hin,content);
+    // cout<<"This is the content in the file : "<<content<<endl;
+
+    // |--------------------------PART 3 ---------------------------------------------------|
+
+    ofstream out;
+    out.open("sample.txt");
+    out<<"This is my file ";
+    out<<"Who I am \n";
+    out<<"Also";
+    out.close();
+
+    ifstream in;
+    string st,st2;
+    in.open("sample.txt");
+    // in>>st>>st2;
+    // cout<<st<<st2;
+    while(in.eof()==0){  //eof() only becomes true if you have gone past EOF
+        in>>st;
+        cout<<st;
+    }     
+    in.close();
 
     return 0;
 }
