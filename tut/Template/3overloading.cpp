e350b2774f63e6950f3e305918738bf39ff8d0cf
Armaan <armaanbhatoya390@gmail.com>
2025-11-16 19:59:00 +0530

    modify

--- tut/Template/3overloading.cpp
+++ tut/Template/3overloading.cpp
@@ -12,14 +12,27 @@ class Armaan{
         void display();
 };
 
-template<class T>
+template<class T>              // ************
 void Armaan<T>:: display(){
-    cout<< data;
+    cout<< data<<endl;
 }
+
+//-----------------------------template function overloading
+void func(int a){
+    cout<<"I am first func()"<<a<<endl;
+}
+
+template<class T>
+void func(T a){
+    cout<<"I am templatised func()"<<a<<endl;
+}
+
 
 int main(){
     Armaan<int> A(22);
     A.display();
 
+    func(5);      //Exact match takes the highest priority
+    func(5.26);
     return 0;
 }
