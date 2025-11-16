326d7eee93493a9d2668b55c5b98f29a6bf25310
Armaan <armaanbhatoya390@gmail.com>
2025-11-16 13:24:10 +0530

    modified

--- tut/file_handling/main.cpp
+++ tut/file_handling/main.cpp
@@ -5,17 +5,26 @@ using namespace std;
 
 int main(){
 
-    string st = "Armaan";
-    string st2;
-    // // opening file using constructor and writing it
-    // ofstream out("sample.txt"); //write operation
-    // out<<st;
+    // string st = "Armaan";
+    // string st2;
+    // // // opening file using constructor and writing it
+    // // ofstream out("sample.txt"); //write operation
+    // // out<<st;
 
-    ifstream in("sample.txt"); //opening file using constructor and reading it
-    in>>st2;
-    cout<<st2;
-    getline(in, st2); //it gets the whole line with spaces
-    cout<<st2;
-    
+    // ifstream in("sample.txt"); //opening file using constructor and reading it
+    // in>>st2;
+    // cout<<st2;
+    // getline(in, st2); //it gets the whole line with spaces
+    // cout<<st2;
+
+    // -------------------- part 2 ---------------------------------
+
+    ofstream hout("sample.txt");
+    string st;
+    cout<<"Enter your name : ";
+    getline(cin,st);
+    hout<<"My name is "<<st;
+    hout.close();
+
     return 0;
 }
