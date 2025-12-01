5251f54f377c7a01d54e8b94743c8673c78ddde8
Armaan Bhatoya <armaanbhatoya390@gmail.com>
2025-12-01 22:14:49 +0530

    modified

--- tut/tempCodeRunnerFile.cpp
+++ tut/tempCodeRunnerFile.cpp
@@ -1,3 +1,30 @@
- // Teacher(){  //unparameterised constructor
-        //     cout<<"Hi i am constructor\n";
-        // }
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    Base() {
+        cout << "Base Constructor\n";
+    }
+     ~Base() {               // ← NOT virtual
+        cout << "Base Destructor\n";
+    }
+};
+
+class Derived : public Base {
+public:
+    Derived() {
+        cout << "Derived Constructor\n";
+    }
+    ~Derived() {
+        cout << "Derived Destructor\n";
+    }
+};
+
+int main() {
+    Base* ptr;  
+    Derived d;
+    ptr = &d;
+   // ← IMPORTANT LINE
+    return 0;
+}
