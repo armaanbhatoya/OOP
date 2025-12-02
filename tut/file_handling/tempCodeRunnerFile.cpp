7b7ada1e54b538f086862e5cb8d39be093f567d0
Armaan Bhatoya <armaanbhatoya390@gmail.com>
2025-12-02 16:58:43 +0530

    modified

--- tut/file_handling/tempCodeRunnerFile.cpp
+++ tut/file_handling/tempCodeRunnerFile.cpp
@@ -1,3 +1,21 @@
- // // opening file using constructor and writing it
-    // ofstream out("sample.txt"); //write operation
-    // out<<st;
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    ifstream file("example.txt", ios::in);
+
+    file.seekg(0, ios::end);
+    char c;
+    file.get(c);
+    cout<<c;
+    int size = file.tellg();
+    file.seekg(-2,ios::end);
+    file.get(c);
+    cout<<c;
+    file.seekg(0, ios::beg);
+
+    cout << "File size = " << size << " bytes\n";
+
+    return 0;
+}
